7a078c5d8b28992232a6c89dbb8cde20561fc5b6
Kevin Moura <brawlfink0005@gmail.com>
2025-07-18 22:04:52 +0000

    coloque isso debaixo do rodape do site de forma discrieta e adaptanda as

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -15,6 +15,14 @@ export function FooterSection() {
           </Link>
         </div>
       </div>
+      <div className="container mx-auto max-w-5xl text-center text-xs text-muted-foreground/70 mt-8 space-y-4 px-4">
+        <p>
+          ESTE SITE NÃO É do FACEBOOK: Este site não faz parte do site do Facebook ou do Facebook Inc. Além disso, este site NÃO é endossado pelo Facebook de nenhuma maneira. FACEBOOK é uma marca comercial independente da FACEBOOK, Inc.
+        </p>
+        <p>
+          Os resultados referentes ao serviço foram avaliados e comprovados por nossos clientes. Não garantimos os resultados iguais para todos, estes variam de pessoa para pessoa dependendo de diversos fatores individuais. As informações fornecidas neste site destina-se ao seu conhecimento geral e não garantimos um resultado específico, todo resultado será de inteira responsabilidade do cliente. Essa página/produto não tem vínculo com o Facebook.
+        </p>
+      </div>
     </footer>
   );
 }
