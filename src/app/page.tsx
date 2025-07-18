d5ae38840ec5b81fc5be947f8e76944be0dac4e5
Kevin Moura <brawlfink0005@gmail.com>
2025-07-18 04:23:41 +0000

    coloque  Garanta Estes Bônus Exclusivosabaixo da oferta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
         <PersuasaoOcultaSection />
         <ResultadosSection />
         <OfertaSection />
-        <DepoimentosSection />
         <BonusSection />
+        <DepoimentosSection />
         <PerguntasRespostasSection />
         <GarantiaSection />
         <FinalCtaSection />
