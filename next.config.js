e9f797ecf9573a3cabfa973cc5383b5ffe4bee45
Kevin Moura <brawlfink0005@gmail.com>
2025-07-23 00:32:33 +0000

    Mas no repósitório ta em js e não .ts

---
+++ next.config.js
@@ -0,0 +1,40 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  srcDir: 'src',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'vtvm1.cc.vt.edu',
+      },
+    ],
+    
+  },
+};
+
+module.exports = nextConfig;
