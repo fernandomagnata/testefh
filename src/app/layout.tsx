c7171e0ed36beaa958b567ce3cd7f91e1d435de4
Kevin Moura <brawlfink0005@gmail.com>
2025-11-01 01:52:46 +0000

    nso ts funvcionando o pixellllllllllllllllllllllllllllllllllllllllllllll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,28 +32,30 @@ export default function RootLayout({
           async 
           defer 
         />
-
-        {/* Meta Pixel Code */}
-        <Script id="meta-pixel" strategy="afterInteractive">
+        
+        {/* Utmify Pixel Code */}
+        <Script id="utmify-pixel" strategy="afterInteractive">
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '738618361889095');
-            fbq('track', 'PageView');
+            window.pixelId = "6877de3c73930aaed23043b5";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
           `}
         </Script>
-        {/* End Meta Pixel Code */}
+
+        {/* Utmify UTMs Script */}
+        <Script
+          id="utmify-utms"
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck
+          data-utmify-prevent-subids
+          async
+          defer
+        ></Script>
       </head>
       <body className="font-body antialiased" suppressHydrationWarning={true}>
-        <noscript><img height="1" width="1" style={{display: 'none'}}
-          src="https://www.facebook.com/tr?id=738618361889095&ev=PageView&noscript=1"
-        /></noscript>
         {children}
         <Toaster />
       </body>
